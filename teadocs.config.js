de88152eb01eb9e2b0a1dad2937ea44c9caa1d1e
lisniuse <17560235@qq.com>
2018-11-28 16:11:49 +0800

    update.

--- teadocs.config.js
+++ teadocs.config.js
@@ -19,6 +19,45 @@ module.exports = {
         <link rel="shortcut icon" href="/static/favicon.ico"/>
         `,
         footHtml: `
+        <script>
+        var _hmt = _hmt || [];
+        (function() {
+          var hm = document.createElement("script");
+          hm.src = "https://hm.baidu.com/hm.js?198a327b7394c4873952b3dc378df8c0";
+          var s = document.getElementsByTagName("script")[0]; 
+          s.parentNode.insertBefore(hm, s);
+        })();
+        </script>
+        <script>
+            var comments = window.document.createElement("div");
+            comments.style.maxWidth = "900px";
+            comments.style.backgroundColor = "#fff";
+            comments.style.padding = "20px";
+            comments.style.boxSizing = "border-box";
+            comments.id = "comments";
+            document.querySelector(".tea-container").appendChild(comments);
+        </script>
+        <script>
+        (function() {
+            var ipc = window.document.createElement("div");
+            ipc.id = "ipcBox";
+            ipc.style.fontSize = "12px";
+            ipc.style.maxWidth = "900px";
+            ipc.style.padding = "20px";  
+            ipc.style.boxSizing = "border-box";
+            ipc.style.margin = "0px";
+            ipc.style.textAlign = "center";
+            ipc.style.backgroundColor = "#fff";
+            ipc.innerHTML = "<span style='color: #bdbdbd;'>@2018 pypandas.cn </span><a style='color: #bdbdbd;' href='http://www.miitbeian.gov.cn/' target='_blank'>粤ICP备16025085号-8</a>"
+            document.querySelector(".tea-container").appendChild(ipc);
+        })();
+        </script>
+        <script>
+            var script = document.createElement("script");
+            script.src = "https://code.tellto.cn/dist/js/init.min.js";
+            script.setAttribute('data-el', '#comments');
+            document.body.appendChild(script); 
+        </script>        
         `,
         isMinify: true, 
         rootPath: "/"
