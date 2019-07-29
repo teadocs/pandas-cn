1e272ac3db177cdaf5348bc3d058fb8361e134ef
lisniuse <17560235@qq.com>
2019-07-29 13:51:48 +0800

    更新网站主题版本。

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -77,7 +77,7 @@ module.exports = ctx => ({
     alert: [{
       id: '2019-7-29',
       title: '文档公告',
-      content: `我们经常发布文档更新，此页面的翻译可能仍在进行中。有关最新信息，请访问<a href="/en/">英文文档</a>。如果此页面上的翻译有问题，请提issues<a href="https://github.com/teadocs/pandas-cn/issues" target="_blank">告诉我们</a>。`
+      content: `我们经常发布文档更新，部分页面的翻译可能仍在进行中。有关最新信息，请访问<a href="/en/">英文文档</a>。如果某个页面上的翻译有问题，请提issues<a href="https://github.com/teadocs/pandas-cn/issues" target="_blank">告诉我们</a>。`
     }],
     repo: 'teadocs/pandas-cn',
     editLinks: true,
