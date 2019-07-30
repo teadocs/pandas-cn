be14ddcf08c402acdd2e4971e277ac8f3bc2d0dd
lisniuse <17560235@qq.com>
2019-07-30 12:26:17 +0800

    新增awesome导航栏目。

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -13,10 +13,18 @@ module.exports = ctx => ({
     }
   },
   head: [
-    ['link', { rel: 'dns-prefetch', href: `//cdn.bootcss.com` }],
-    ['link', { rel: 'dns-prefetch', href: `//cdn.mathjax.org` }],
+    ['link', {
+      rel: 'dns-prefetch',
+      href: `//cdn.bootcss.com`
+    }],
+    ['link', {
+      rel: 'dns-prefetch',
+      href: `//cdn.mathjax.org`
+    }],
     // 使主题能够支持数学公式
-    ['script', {type: 'text/x-mathjax-config'}, `
+    ['script', {
+      type: 'text/x-mathjax-config'
+    }, `
     MathJax.Hub.Config({
       showProcessingMessages: false, //关闭js加载过程信息
       messageStyle: "none", //不显示信息
@@ -32,7 +40,9 @@ module.exports = ctx => ({
       }
     })
     `],
-    ['script', {src: '//cdn.bootcss.com/mathjax/2.7.0/MathJax.js?config=TeX-AMS-MML_HTMLorMML'}],
+    ['script', {
+      src: '//cdn.bootcss.com/mathjax/2.7.0/MathJax.js?config=TeX-AMS-MML_HTMLorMML'
+    }],
     // 监听路由重新渲染数学公式
     ['script', {}, `
       (function() {
@@ -48,15 +58,43 @@ module.exports = ctx => ({
         }, 200);
       })();
     `],
-    ['link', { rel: 'icon', href: `/logo.png` }],
-    ['link', { rel: 'manifest', href: '/manifest.json' }],
-    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
-    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-    ['link', { rel: 'apple-touch-icon', href: `/icons/apple-touch-icon-152x152.png` }],
-    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
-    ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
-    ['meta', { name: 'msapplication-TileColor', content: '#000000' }],
+    ['link', {
+      rel: 'icon',
+      href: `/logo.png`
+    }],
+    ['link', {
+      rel: 'manifest',
+      href: '/manifest.json'
+    }],
+    ['meta', {
+      name: 'theme-color',
+      content: '#3eaf7c'
+    }],
+    ['meta', {
+      name: 'apple-mobile-web-app-capable',
+      content: 'yes'
+    }],
+    ['meta', {
+      name: 'apple-mobile-web-app-status-bar-style',
+      content: 'black'
+    }],
+    ['link', {
+      rel: 'apple-touch-icon',
+      href: `/icons/apple-touch-icon-152x152.png`
+    }],
+    ['link', {
+      rel: 'mask-icon',
+      href: '/icons/safari-pinned-tab.svg',
+      color: '#3eaf7c'
+    }],
+    ['meta', {
+      name: 'msapplication-TileImage',
+      content: '/icons/msapplication-icon-144x144.png'
+    }],
+    ['meta', {
+      name: 'msapplication-TileColor',
+      content: '#000000'
+    }],
     // 百度统计
     ['script', {}, `
       (function() {
@@ -105,7 +143,16 @@ module.exports = ctx => ({
     ['@vuepress/back-to-top', true],
     ['@vuepress/pwa', {
       serviceWorker: true,
-      updatePopup: true
+      updatePopup: {
+        '/': {
+          message: "发现新内容可用",
+          buttonText: "刷新"
+        },
+        '/en/': {
+          message: "New content is available.",
+          buttonText: "Refresh"
+        }
+      }
     }],
     ['@vuepress/medium-zoom', true],
     ['container', {
